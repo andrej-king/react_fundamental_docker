808f0ab1cfa4b31f7c961c1c8f6f0c3830af6fe4
Andrey <graduadu@gmail.com>
2021-10-12 16:33:08 +0300

    Added basic hooks.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React, {useCallback, useContext, useEffect, useMemo, useState} from 'react'
+
+// basic hooks
+// useState()
+// useEffect()
+// useRef()
+// useMemo()
+// useCallback()
+// useContext()
 
 function App() {
     return (
