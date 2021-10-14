b7b51e8756589ff150f27cb72e0a919cf4b6db58
Andrey <graduadu@gmail.com>
2021-10-14 15:46:52 +0300

    Moved routes in separate array with objects

--- app/src/components/AppRouter.jsx
+++ app/src/components/AppRouter.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
-import About from '../pages/About'
-import Posts from '../pages/Posts'
-import PostIdPage from '../pages/PostIdPage'
-import Error from '../pages/Error'
 import {
     Switch,
     Route,
     Redirect
 } from "react-router-dom";
+import {routes} from '../router/routes'
 
 const AppRouter = () => {
     return (
         <Switch>
-            <Route path="/about">
-                <About/>
-            </Route>
-            <Route exact path="/posts">
-                <Posts/>
-            </Route>
-            <Route exact path="/posts/:id">
-                <PostIdPage/>
-            </Route>
-            <Route path="/error">
-                <Error/>
-            </Route>
+            {routes.map(route =>
+                <Route
+                    component={route.component}
+                    path={route.path}
+                    exact={route.exact}
+                />
+            )}
             <Redirect to='/error'/>
         </Switch>
     );
